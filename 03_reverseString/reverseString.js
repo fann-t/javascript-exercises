b968a16d84820f5b938a4ea09c52c6ab0fbc6c6e
viv <31870588+viv-t@users.noreply.github.com>
2024-02-15 11:14:55 -0800

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let newstr = "";
+    for (let i = 0; i < s.length; i++){
+        newstr = s[i] + newstr;
+    }
+    return newstr;
 };
 
 // Do not edit below this line
