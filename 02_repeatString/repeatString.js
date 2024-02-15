b9551c8582ec613d4fde2ab6c5a382ba525dad9f
viv <31870588+viv-t@users.noreply.github.com>
2024-02-15 10:53:31 -0800

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(mystr, count) {
 
+    if (count < 0){
+        return "ERROR";
+    } else {
+        let newstring = "";
+        for (let i = 0; i < count; i++) {
+            newstring += mystr;
+        } 
+        return newstring;
+    }
 };
 
 // Do not edit below this line
