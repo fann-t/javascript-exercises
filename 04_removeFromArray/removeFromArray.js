5be167c3fe99cd2d3ab26453dec4b5763ee7ecd2
viv <31870588+viv-t@users.noreply.github.com>
2024-02-15 17:07:09 -0800

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(a) {
+    let array = arguments[0];
+    let remove = Array.prototype.slice.call(arguments, 1);
+
+    for (let i = 0; i < remove.length; i++){
+        while (array.includes(remove[i])){
+            var index = array.indexOf(remove[i]);
+
+
+            array.splice(index, 1);
+        }
+    }
+    return array;
 
 };
 
