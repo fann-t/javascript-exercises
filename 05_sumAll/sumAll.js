b1628f5b4b73aed757734fd5632f1d9e85f5d56d
viv <31870588+viv-t@users.noreply.github.com>
2024-02-16 00:04:07 -0800

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a) {
+    let start = arguments[0];
+    let end = arguments[1];
 
+    if (typeof end === "number" && start > 0) {
+        if (start > end){
+            let tmp = start;
+            start = end;
+            end = tmp;
+        }
+        let sum = 0;
+        for (let i = start ; i < end + 1; i++ ) {
+            sum += i;
+        }
+        return sum;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
