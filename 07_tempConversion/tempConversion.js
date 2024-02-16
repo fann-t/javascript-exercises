9142cc65d3319239bfeb07734a6e620e3f306a8b
viv <31870588+viv-t@users.noreply.github.com>
2024-02-16 00:16:48 -0800

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree_f) {
+  return Math.round((degree_f -32) * 5/9 * 10) / 10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree_c) {
+  return Math.round((degree_c * 9 /5 +32) * 10) / 10;
 };
 
 // Do not edit below this line
